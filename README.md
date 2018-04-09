21c0d0ab7ecdd26406ca0b114e5c21eed69cbb18
MarcServat <marcservat@hotmail.com>
2018-04-09 21:37:15 +0200

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # trailer_movies
+
+A website which show trailer movies aimed to practice with python and classes
--- entertainment_center.py
+++ entertainment_center.py
@@ -1,18 +1,21 @@
 import media
+import movies
 
-toy_story = media.Movie("Toy Story", 
-    "A history of a boy who his imagination run wild",
-    "https://es.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.svg",
-    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+irreversible = media.Movie("Irrversible", 
+    "Sex and violence, HOT topic",
+    "http://www.fotogramas.es/var/ezflow_site/storage/images/peliculas/irreversible/110105-1-esl-ES/Irreversible1_cartel.jpg",
+    "https://www.youtube.com/watch?v=j7EDpDba8bU")
 
-avatar = media.Movie("Avatar",
-    "A marine who walks through the jungle",
-    "https://es.wikipedia.org/wiki/Avatar_(pel%C3%ADcula)#/media/File:Avatar-Logo-avatar.svg",
-    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+requiem = media.Movie("Requiem",
+    "American psychological drama",
+    "https://pics.filmaffinity.com/requiem_for_a_dream-174867645-large.jpg",
+    "https://www.youtube.com/watch?v=0nU7dC9bIDg")
 
-interstellar = media.Movie("Interstellar",
-    "a multi dimension future collapse into enviromental issues",
-    "https://en.wikipedia.org/wiki/Interstellar_(film)#/media/File:Interstellar_film_poster.jpg",
-    "https://www.youtube.com/watch?v=1weAVdHUNlk") 
+apocalypto = media.Movie("Apocalypto",
+    "As the Mayan kingdom faces its decline, a young man is taken on a perilous journey to a world ruled by fear and oppression.",
+    "https://ia.media-imdb.com/images/M/MV5BMzhmNGMzMDMtZDM0Yi00MmVmLWExYjAtZDhjZjcxZDM0MzJhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
+    "https://www.youtube.com/watch?v=ngWBddVNVZs") 
 
-interstellar.show_trailer()
+movies_list = [irreversible, requiem, apocalypto]
+
+movies.open_movies_page(movies_list)
